c34e283f682b5ae3fe26dce161a6763087e21840
nabi-ozturk <nabiozturk34@gmail.com>
2024-03-21 03:17:55 +0300

    Düzenleme
    Düzenlemeler yapıldı

--- Oyun Gelistirme Projeleri/Oyun_Gelistirme_Ders_1/Assets/Ders 2/Kodlar/ikinciders.cs
+++ Oyun Gelistirme Projeleri/Oyun_Gelistirme_Ders_1/Assets/Ders 2/Kodlar/ikinciders.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ikinciders : MonoBehaviour
+
+
+
+    
 {
+
+    public Text helloWorld;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        helloWorld.text = "Merhaba Dünya!";
 
+        Debug.Log("Ýnspimo Consultancy");
+    }
+    
     // Update is called once per frame
     void Update()
     {
