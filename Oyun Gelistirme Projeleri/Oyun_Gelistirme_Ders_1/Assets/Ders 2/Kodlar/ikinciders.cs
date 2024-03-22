1dd62806b2728aaaed62e91ff35d4840c8d21e95
nabi-ozturk <nabiozturk34@gmail.com>
2024-03-22 16:31:46 +0300

    Değişiklikler
    Değişiklikler yapıldı

--- Oyun Gelistirme Projeleri/Oyun_Gelistirme_Ders_1/Assets/Ders 2/Kodlar/ikinciders.cs
+++ Oyun Gelistirme Projeleri/Oyun_Gelistirme_Ders_1/Assets/Ders 2/Kodlar/ikinciders.cs
@@ -11,17 +11,36 @@ public class ikinciders : MonoBehaviour
 {
 
     public Text helloWorld;
+    public Text sayacText;
+
+    public Image sayacResmi;
+
+    public float sayac = 10;
     // Start is called before the first frame update
     void Start()
     {
         helloWorld.text = "Merhaba Dünya!";
 
-        Debug.Log("Ýnspimo Consultancy");
+        Debug.Log("Oyun Baþladý-START");
     }
     
     // Update is called once per frame
     void Update()
     {
         
+        if (sayac>0)
+        {
+            sayac = sayac - Time.deltaTime;
+            sayacText.text = sayac.ToString();
+            helloWorld.text = "Ders Baþlýyor...";
+        }
+        else if(sayac<=0)
+        { 
+        
+        }
+        sayac = 0;
+        sayacResmi.gameObject.SetActive(false);
+        sayacText.text = "";
+        helloWorld.text = "Ders Baþladý!..";
     }
 }
