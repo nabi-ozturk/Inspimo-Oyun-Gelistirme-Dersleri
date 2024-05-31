7d788cef6d9c2aec0838d0e977f4f4ec9cfcb1e1
nabi-ozturk <nabiozturk34@gmail.com>
2024-05-31 18:01:06 +0300

    Hafta 4
    Hafta 4, seçim ekranına kadar kaydedildi

--- Oyun Gelistirme Projeleri/Oyun_Gelistirme_Ders_1/Assets/Ders 4/hafta4.cs
+++ Oyun Gelistirme Projeleri/Oyun_Gelistirme_Ders_1/Assets/Ders 4/hafta4.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class hafta4 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text isimSoyisim;
+    public Text adres;
+    public Text webSitesi;
+    public Text telefon;
+    public Text email;
+    public Text biyografi;
+
+    public Image resim; 
+    public Sprite profilFotografi;
     void Start()
+    {
+        biyografi.text = "Bilgi çekilirken hata oluþtu, lütfen tekrar deneyiniz!";
+        resim.gameObject.SetActive(false);
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void veriÇek() {
+
+
+        resim.gameObject.SetActive(true);
+
+
+
+        isimSoyisim.text = "Muhammed Nabi Öztürk";
+        adres.text = "Fýrat Teknokent Ofis:221 \nMerkez/Elazýð";
+        webSitesi.text = "www.nabiozturk.com";
+        telefon.text = "0555 555 55 55";
+        email.text = "nabiozturk34@gmail.com";
+        biyografi.text = "Fýrat Üniversitesi Teknoloji Fakültesi Yazýlým Mühendisliði 1. Sýnýf Öðrencisi";
+
+        resim.sprite = profilFotografi;
+
+
+
+
+
+
+
+
     }
 }
